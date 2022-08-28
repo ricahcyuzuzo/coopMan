11dfdbe770f41051e73db12a144381023b6cede6
Richard NSHIMYUMUKIZA <richcyuzuzo1@gmail.com>
2022-08-28 12:18:04 +0200

    hh

--- server/app.js
+++ server/app.js
@@ -16,10 +16,10 @@ app.use('/api', coopRoutes);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, content-type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', true)
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
   }
   return next();
